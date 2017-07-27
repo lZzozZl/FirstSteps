a0f34716df0fb40aff9842216ec893965f5b91a3
r.yordanov89 <r.yordanov89@gmail.com>
2017-07-27 05:23:15 -0400

    Small js for accordion

---
+++ Training projects/Avonmore/script.js
@@ -0,0 +1,22 @@
+function acc(x) {
+  if (x.getAttribute('id') === "accOne") {
+    document.getElementById('pOne').style.display = "block";
+    document.getElementById('pTwo').style.display = "none";
+    document.getElementById('pThree').style.display = "none";
+  } 
+  else if (x.getAttribute('id') === "accTwo") {
+    document.getElementById('pOne').style.display = "none";
+    document.getElementById('pTwo').style.display = "block";
+    document.getElementById('pThree').style.display = "none";
+    
+    
+  } 
+  else if (x.getAttribute('id') === "accThree") {
+    document.getElementById('pOne').style.display = "none";
+    document.getElementById('pTwo').style.display = "none";
+    document.getElementById('pThree').style.display = "block";   
+    
+  }
+  
+  
+}
